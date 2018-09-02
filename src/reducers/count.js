fbde6351e2584732b8597befe2e80bd3903b6039
hirontan <hirontan@sora.flights>
2018-09-02 22:04:29 +0900

    correct type miss

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,8 +1,8 @@
-import { INCREAMENT, DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT } from '../actions'
 
 const initialState = { value: 0 }
 
-export default (state = intialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return { value: state.value + 1 }
